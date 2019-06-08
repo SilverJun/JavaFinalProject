22122d2cd496870d1d457c6368c8b60325ec7a79
SilverJun <jangej1031@naver.com>
2019-06-08 22:54:51 +0900

    upload util

--- src/main/java/edu/handong/java/utils/Util.java
+++ src/main/java/edu/handong/java/utils/Util.java
@@ -1,5 +1,16 @@
 package edu.handong.java.utils;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+
 public class Util {
 	public static String getExtension(String path)
 	{
@@ -9,4 +20,53 @@ public class Util {
 	{
 		return path.substring(0, path.lastIndexOf("."));
 	}
+	
+	/**
+	 * 출처 : https://javafreak.tistory.com/226
+	 * @param dir
+	 * @throws IOException
+	 */
+	private static void ensureDestDir(File dir) throws IOException {
+		if ( ! dir.exists() ) {
+			dir.mkdirs(); /*  does it always work? */
+		}
+	}
+	/**
+	 * 출처 : https://javafreak.tistory.com/226
+	 * @param zip
+	 * @param destDir
+	 * @throws IOException
+	 */
+	public static void unzip(File zip, File destDir) throws IOException {
+		InputStream is = new FileInputStream(zip);
+		String encoding = Charset.defaultCharset().name();
+		
+		ZipArchiveInputStream zis ;
+		ZipArchiveEntry entry ;
+		String name ;
+		File target ;
+		int nWritten = 0;
+		BufferedOutputStream bos ;
+		byte [] buf = new byte[1024 * 8];
+
+		ensureDestDir(destDir);
+		
+		zis = new ZipArchiveInputStream(is, encoding, false);
+		while ( (entry = zis.getNextZipEntry()) != null ){
+			name = entry.getName();
+			target = new File (destDir, name);
+			if ( entry.isDirectory() ){
+				ensureDestDir(target);
+			} else {
+				target.createNewFile();
+				bos = new BufferedOutputStream(new FileOutputStream(target));
+				while ((nWritten = zis.read(buf)) >= 0 ){
+					bos.write(buf, 0, nWritten);
+				}
+				bos.close();
+			}
+		}
+		zis.close();
+	}
+	
 }
